0200d1c4910841012bb6db8dd27ef47b64e8426e
shatteringlass <federico.pizzolo@gmail.com>
2024-01-29 23:41:59 +0100

    Day 24 pt.1

--- solutions/day24.py
+++ solutions/day24.py
@@ -1,19 +1,20 @@
 import dataclasses
 from util import read_input
+import fractions
 
 TEST_AREA = ((200_000_000_000_000, 200_000_000_000_000),
              (400_000_000_000_000, 400_000_000_000_000))
 
 
 """
-The system dynamic is described by the following system of equations:
+Each stone moves along the line defined by the following parametric equation:
 
 P(t) = P + V*t
 
-In part 1, reduce to a n**2 system of equations and compute 
-the number of linearly independent vectors that intersect in the test area.
+where P is position and V is velocity.
 
 """
+
 
 @dataclasses.dataclass
 class Hailstone:
@@ -21,11 +22,67 @@ class Hailstone:
     position: tuple[int, int, int]
     velocity: tuple[int, int, int]
 
+    @property
+    def px(self):
+        return self.position[0]
+
+    @property
+    def py(self):
+        return self.position[1]
+
+    @property
+    def pz(self):
+        return self.position[2]
+
+    @property
+    def vx(self):
+        return self.velocity[0]
+
+    @property
+    def vy(self):
+        return self.velocity[1]
+
+    @property
+    def vz(self):
+        return self.velocity[2]
+
+    def position_at_time(self, t):
+        return (self.position[i] + t * self.velocity[i] for i, _ in enumerate(self.position))
+
+    def intersects(self, other):
+
+        det = self.vy*other.vx - self.vx*other.vy
+        if det != 0:
+            t1 = (other.vx*(other.py-self.py) -
+                  other.vy*(other.px-self.px))/det
+            t2 = (self.vx*(other.py-self.py) - self.vy*(other.px-self.px))/det
+            intersect = (t1, t2)
+            #print(f"Line {self} intersects {other} at {intersect}")
+        else:
+            intersect = None
+
+        return intersect
+
     @classmethod
     def from_str(cls, string):
         position, velocity = tuple(
             tuple(int(y) for y in x.split(',')) for x in string.split(" @ "))
         return Hailstone(position, velocity)
+
+
+def check_intersections(stones, min_value, max_value):
+    intersections = 0
+
+    for idx, stone in enumerate(stones):
+        for jdx, other_stone in enumerate(stones[idx+1:]):
+            intersect = stone.intersects(other_stone)
+            if intersect:
+                t0, t1 = intersect
+                pxt, pyt, pyz = stone.position_at_time(intersect[0])
+                if t0 > 0 and t1 > 0 and (min_value <= pxt <= max_value) and (min_value <= pyt <= max_value):
+                    intersections += 1
+
+    return intersections
 
 
 def get_solution(part):
@@ -35,10 +92,9 @@ def get_solution(part):
 
     stones = tuple(Hailstone.from_str(line) for line in data)
 
-    input(stones)
-
     if part == 1:
-        pass
+        solution = check_intersections(
+            stones, TEST_AREA[0][0], TEST_AREA[1][1])
     elif part == 2:
         pass
 
